f9e9bcf12bd09e02180b6a925bded66d499a36ba
Zeyu Chen <ZYCM03@gmail.com>
2020-04-01 09:27:44 -0400

    Create Week1_Day01_Single_Number.py

---
+++ Week1_Day01_Single_Number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        # Approach 1: set()
+		#     This will work in the general case, but memory complexity will become O(N)
+		#     Although set operations in Python are O(1), the slowdown will become visible after 10^6 items.
+		
+		# Approach 2: Bit manipulation with XOR
+        
+        # X ^ X == 0, all the items that appear twice become 0
+        # 0 ^ 0 == 0, in the end, we are left with 0 if every item appears twice
+        # X ^ 0 == X, the item that appears once is the odd one out
+        
+        dup = 0
+        for val in nums:
+            dup ^= val
+        return dup
