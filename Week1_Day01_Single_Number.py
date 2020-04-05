040900ca252413219c20f48e1857d7e6df89b05f
Zeyu Chen <ZYCM03@gmail.com>
2020-04-05 15:34:02 -0400

    Fixed indenting

--- Week1_Day01_Single_Number.py
+++ Week1_Day01_Single_Number.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         
         # Approach 1: set()
-		#     This will work in the general case, but memory complexity will become O(N)
-		#     Although set operations in Python are O(1), the slowdown will become visible after 10^6 items.
+		#   This will work in the general case, but memory complexity will become O(N)
+		#   Although set operations in Python are O(1), the slowdown will become visible after 10^6 items as there is a little bit of overhead.
 		
 		# Approach 2: Bit manipulation with XOR
         
